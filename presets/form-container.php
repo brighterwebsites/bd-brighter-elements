59fc431b4d087d3ef59cc39846a0524a938c3d5f
Vanessa Wood <brighterwebsitedesign@gmail.com>
2026-05-12 16:08:31 +1000

    Add files via upload
    form elements

---
+++ presets/form-container.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace BreakdanceCustomElements;
+
+use function Breakdance\Elements\c;
+use function Breakdance\Elements\PresetSections\getPresetSection;
+
+\Breakdance\Elements\PresetSections\PresetSectionsController::getInstance()->register(
+    "BreakdanceCustomElements\\form-container",
+    c(
+        "container",
+        "Container",
+        [c(
+        "width",
+        "Width",
+        [],
+        ['type' => 'unit', 'layout' => 'inline'],
+        true,
+        false,
+        [],
+        
+      ), c(
+        "background",
+        "Background",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+        
+      ), getPresetSection(
+      "EssentialElements\\spacing_padding_all",
+      "Padding",
+      "padding",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\borders",
+      "Borders",
+      "borders",
+       ['type' => 'popout']
+     )],
+        ['type' => 'section', 'sectionOptions' => ['type' => 'popout', 'preset' => ['slug' => 'BreakdanceCustomElements\\form-container']]],
+        false,
+        false,
+        [],
+        
+      ),
+    true,
+    null
+);
+
